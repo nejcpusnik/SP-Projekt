22ce2d82d5dc4ba14fb04d54ade80ebb516fce5a
nejcpusnik <np4518@student.uni-lj.si>
2016-11-07 00:15:16 +0100

    Navigation bar added

--- JS/script.js
+++ JS/script.js
@@ -2,4 +2,18 @@
  * Created by Puska on 30. 10. 2016.
  */
 
+function initMap() {
+    var uluru = {lat: 46.0552778, lng: 14.5144444};
+    var map = new google.maps.Map(document.getElementById('map'), {
+        zoom: 10,
+        center: uluru
+    });
+    var marker = new google.maps.Marker({
+        position: uluru,
+        map: map
+    });
+}
 
+window.onload = function() {
+    initMap();
+};
