321ee28e0da9d4d8b43698473ea32f59b58c1e64
nejcpuska <neic.pusnik@gmail.com>
2016-11-08 22:03:15 +0100

    fixed margin

---
+++ JS/landingscript.js
@@ -0,0 +1,21 @@
+function myFunction() {
+    var x = document.getElementById("myTopnav");
+    if (x.className === "topnav") {
+        x.className += " responsive";
+    } else {
+        x.className = "topnav";
+    }
+}
+
+var myNav = document.getElementById('myTopnav');
+window.onscroll = function () { 
+    "use strict";
+    if (document.body.scrollTop >= 1 ) {
+        myNav.classList.add("topnav");
+        myNav.classList.remove("topnav-transparent");
+    } 
+    else {
+        .add("topnav-transparent");
+        .remove("topnav");
+    }
+};
