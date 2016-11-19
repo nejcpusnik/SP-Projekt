ac00c930d49c1368f581a963c3566f471efe1f19
nejcpuska <neic.pusnik@gmail.com>
2016-11-19 20:52:17 +0100

    added login page, responsive design

--- JS/mapscript.js
+++ JS/mapscript.js
@@ -1,6 +1,14 @@
 /**
  * Created by Puska on 30. 10. 2016.
  */
+function responsiveNav() {
+    var x = document.getElementById("myTopnav");
+    if (x.className === "topnav") {
+        x.className += " responsive";
+    } else {
+        x.className = "topnav";
+    }
+}
 
 function initMap() {
     var uluru = {lat: 46.0552778, lng: 14.5144444};
