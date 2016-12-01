9355de676ffef87e8f04ec01bbb58bc4384fffcb
nejcpusnik <np4518@student.uni-lj.si>
2016-12-01 21:38:32 +0100

    change slides

--- JS/combinescript.js
+++ JS/combinescript.js
@@ -15,7 +15,7 @@ var slideIndex = 0;
 function showSlides() {
     var i;
     var slides = document.getElementsByClassName("mySlides");
-    var dots = document.getElementsByClassName("dot");
+
     for (i = 0; i < slides.length; i++) {
         slides[i].style.display = "none";
     }
